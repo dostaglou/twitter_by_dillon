bc22aefd395988ef141de60a8584b22b715c6987
dillon wyatt <dostaglou@gmail.com>
2019-09-22 18:29:29 +0900

    styled devise forms a bit - added bio section

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 
-  resources :users
+  resources :users, only: [:index, :show, :edit, :update]
   resources :tweets
-
 
 end

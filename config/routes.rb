5da9c7565a65fb2b43f2c7bb7cf9b8f72b65268b
dillon wyatt <dostaglou@gmail.com>
2019-09-22 14:29:28 +0900

    created simple landing page with root to and links to sign up and in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "tweets#index"
+  root to: "pages#home"
 
   resources :users
   resources :tweets
+
 
 end

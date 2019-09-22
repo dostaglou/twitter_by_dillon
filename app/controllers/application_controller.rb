a1eb122e3ce6e7923a03036cf84ce4e027c68e30
dillon wyatt <dostaglou@gmail.com>
2019-09-22 20:36:51 +0900

    sanitized bio for edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :bio])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:bio])
   end
 end
+
+

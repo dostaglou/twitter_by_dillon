17472b6bca6ed80b71a27c9d4603bb0c3350093c
dillon wyatt <dostaglou@gmail.com>
2019-09-23 18:20:37 +0900

    root is now tweet#index with a render of the landing if not signed in

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+  root to: "tweets#index"
 
   resources :users, only: [:index, :show, :edit, :update]
   resources :tweets

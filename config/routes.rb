09b7afdaaa490d9b7e24b5e13dc20f9a157bc251
dillon wyatt <dostaglou@gmail.com>
2019-09-23 17:50:22 +0900

    basic shape of follow mechinic established

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post ':id/follow_user', to: 'relationships#follow_user', as: :follow_user
+  post ':id/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_130936) do
+ActiveRecord::Schema.define(version: 2019_09_23_071308) do
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true
+    t.index ["following_id"], name: "index_follows_on_following_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id", null: false

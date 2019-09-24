2862f451aed1085c33a49dbf56f99139a9bf56f1
dillon wyatt <dostaglou@gmail.com>
2019-09-24 18:44:13 +0900

    graphql with a Users model

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,14 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :users,
+      [Types::UserType],
+      null: false,
+      description: "returns a list of users in the db"
+
+    def users
+      User.all
     end
+
   end
 end
---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String, null: false
+    field :encrypted_password, String, null: false
+    field :username, String, null: false
+    field :bio, String, null: true
+    field :avatar, String, null: true
+
+  end
+end

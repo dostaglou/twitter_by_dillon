689b8818c335724c1b8fc5d1ab256fdfe3b2e96d
dillon wyatt <dostaglou@gmail.com>
2019-09-29 20:16:57 +0900

    created some basic tests - still have trouble witht the User Controller Tests vis a vis current_user and User.find

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,18 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get users_index_url
+    current_user = User.first
+    #without rewritting view code,
+      # how do I pass current_user without an @?
+    @users = User.all
+    get users_path
     assert_response :success
   end
 
   test "should get show" do
-    get users_show_url
+    get '/users/1'
+    # "ActiveRecord::RecordNotFound: Couldn't find User with 'id'=1"
+    # User 1 exist in my IRB, why not here?
     assert_response :success
   end
 
--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+  test "shouldn't save empty tweet" do
+    tweet = Tweet.new
+    assert_not tweet.save, "empty tweet saved"
+  end
+
+  test "shouldn't save tweet without user" do
+    tweet = Tweet.new(content: "I am a test")
+    tweet.save
+    assert_not(tweet.id.present?, true)
+  end
 end
